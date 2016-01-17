e534bf9a5233bfb551ad64717cff7e5bf324da8f
Bianca Hinova <bianca.hinova@gmail.com>
2016-01-17 22:51:25 +0200

    approve photo presenter

--- ThePhotoChallengeApp/app/src/main/java/com/telerik/airelementalteam/thephotochallengeapp/presenters/main/fragmentPresenters/ApprovePhotoPresenter.java
+++ ThePhotoChallengeApp/app/src/main/java/com/telerik/airelementalteam/thephotochallengeapp/presenters/main/fragmentPresenters/ApprovePhotoPresenter.java
@@ -1,7 +1,27 @@
 package com.telerik.airelementalteam.thephotochallengeapp.presenters.main.fragmentPresenters;
 
-/**
- * Created by bianka on 1/17/16.
- */
+import android.app.Activity;
+import android.app.Fragment;
+
+import com.telerik.airelementalteam.thephotochallengeapp.models.Photo;
+import com.telerik.airelementalteam.thephotochallengeapp.views.fragments.ApprovePhotoFragment;
+
 public class ApprovePhotoPresenter {
+
+    private Activity activity;
+    private ApprovePhotoFragment fragment;
+
+    public ApprovePhotoPresenter(Activity activity, ApprovePhotoFragment fragment) {
+        this.activity = activity;
+    }
+
+    public void takeLocation(Photo photo) {
+    }
+
+    public void onApprovedPhoto(Photo photo) {
+    }
+
+    public void onDeclinedPhoto(Photo photo) {
+
+    }
 }
--- ThePhotoChallengeApp/app/src/main/java/com/telerik/airelementalteam/thephotochallengeapp/views/fragments/ApprovePhotoFragment.java
+++ ThePhotoChallengeApp/app/src/main/java/com/telerik/airelementalteam/thephotochallengeapp/views/fragments/ApprovePhotoFragment.java
@@ -7,11 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.telerik.airelementalteam.thephotochallengeapp.R;
 import com.telerik.airelementalteam.thephotochallengeapp.models.Photo;
+import com.telerik.airelementalteam.thephotochallengeapp.presenters.main.fragmentPresenters.ApprovePhotoPresenter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +22,11 @@ public class ApprovePhotoFragment extends android.app.Fragment
 {
 
     ImageView photoView;
+    ImageView takeLocationButton;
+    ImageView approvePhotoButton;
+    ImageView declinePhotoButton;
+
+    private ApprovePhotoPresenter presenter;
     private Photo photo;
     private Bitmap bmp;
 
@@ -33,8 +40,36 @@ public class ApprovePhotoFragment extends android.app.Fragment
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_approve_photo, container, false);
+        presenter = new ApprovePhotoPresenter(getActivity(), this);
+        takeLocationButton = (ImageView) view.findViewById(R.id.take_location_button);
+        approvePhotoButton = (ImageView) view.findViewById(R.id.approve_photo_button);
+        declinePhotoButton = (ImageView) view.findViewById(R.id.decline_friend_button);
         photoView = (ImageView) view.findViewById(R.id.new_photo_viewer);
         photoView.setImageBitmap(this.bmp);
+
+        takeLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.takeLocation(photo);
+            }
+        });
+
+        approvePhotoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                presenter.onApprovedPhoto(photo);
+
+            }
+        });
+
+        declinePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.onDeclinedPhoto(photo);
+
+            }
+        });
 
         return view;
     }
